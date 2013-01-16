f8a847df0b840628c9415df6f41c2ff8e673295e
Jeffrey Kegler <jkegl@cpan.org>
2013-01-16 00:04:30 +0000

    Import of JKEGL/Marpa-Guide-0.000_000 from CPAN.
    gitpan-cpan-distribution: Marpa-Guide
    gitpan-cpan-version:      0.000_000
    gitpan-cpan-path:         JKEGL/Marpa-Guide-0.000_000.tar.gz
    gitpan-cpan-author:       JKEGL
    gitpan-cpan-maturity:     developer

---
+++ lib/Marpa/Guide.pm
@@ -0,0 +1,38 @@
+package Marpa::Guide;
+
+use 5.010001;
+use strict;
+use warnings;
+
+require Exporter;
+use AutoLoader qw(AUTOLOAD);
+
+our @ISA = qw(Exporter);
+
+# Items to export into callers namespace by default. Note: do not export
+# names by default without a very good reason. Use EXPORT_OK instead.
+# Do not simply export all your public functions/methods/constants.
+
+# This allows declaration	use Marpa::Guide ':all';
+# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
+# will save memory.
+our %EXPORT_TAGS = ( 'all' => [ qw(
+	
+) ] );
+
+our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+
+our @EXPORT = qw(
+	
+);
+
+our $VERSION = '0.000_000';
+$VERSION = eval $VERSION;  # see L<perlmodstyle>
+
+
+# Preloaded methods go here.
+
+# Autoload methods go after __END__, and are processed by the autosplit program.
+
+1;
+__END__
